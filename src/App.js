e482bd1361c1da49b4fae0ee0e4dd5b50fb46d1e
satya kavya <satyakavya24@gmail.com>
2021-04-20 19:28:48 -0500

    created react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+//react component is a java script function, that gets render
+//we can also render html tags
+// function App() {
+//   return <h3>hello world!!!!</h3>
+// }
+function App() {
+  return <div>
+    <h3>hell o world!!!!</h3>
+    <h6>How are you doing</h6>
+    </div>
+}
+   
+
+export default App;
