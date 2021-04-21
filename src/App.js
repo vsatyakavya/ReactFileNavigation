ba71d0d0d47e06fb3fa3314ad99d147855b30a5a
satya kavya <satyakavya24@gmail.com>
2021-04-20 19:45:39 -0500

    created Folder component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
 
 
-//react component is a java script function, that gets render
+//react component is a javascript function, that returns something to be rendered
 //we can also render html tags
+//we are injecting this code into div id="root" in HTML file
+//we can inspect in browser as 
+//<div id="root">
+// <div>
+//  <h3>hello world!!</h3>
+//  <h6>How are you doing</h6>
+// </div>
+//</div>
+
 // function App() {
-//   return <h3>hello world!!!!</h3>
+//   return <div>
+//   <h3>hell o world!!!!</h3>
+//   <h6>How are you doing</h6>
+//   <Folder />  //Advantage of components is, we can render any number of times
+//   <Folder />
+//   <Folder />
+// </div>
 // }
 function App() {
   return <div>
     <h3>hell o world!!!!</h3>
     <h6>How are you doing</h6>
-    </div>
+    <Folder />
+    <Folder />
+    <Folder />
+  </div>
 }
-   
+
+//created a component (Folder) and rendered by (App) component
+//javascript arrow function, which is also a React component
+const Folder = () => {
+  return <h4>Folder</h4>
+}
+
 
 export default App;
